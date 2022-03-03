b6ae51a731dbf9810c0434b8d9a5352b813c472e
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2022-03-03 23:50:07 +0530

    assignment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <ul className='ul'>Mobile Opreation system</ul>
+    <li>Andriod</li>
+    <li>Blackberry</li>
+    <li>Aindows Phone</li>
+    <li>iphone</li>
+
+    <ul className='ul'>Mobile Manufacture</ul>
+    <li>Samsung</li>
+    <li>HTC</li>
+    <li>Micromax</li>
+    <li>Apple</li>
+    
+    </>
+    
+   
+  );
+}
+
+export default App;
